b416ac10558e99a5628205be0c435abd724956cc
ArtisanSelects <85949213+ArtisanSelects@users.noreply.github.com>
2022-03-08 12:53:42 -0500

    Updated SocialContainer

--- src/components/SocialContainer.svelte
+++ src/components/SocialContainer.svelte
@@ -6,7 +6,6 @@
 <div class="row mb-3">
 	<div class="m-auto mt-3 col-sm-12 col-lg-8">
 		<div class="d-flex my-auto justify-content-center">
-			<SocialLink href="https://www.twitter.com/plopadop" src="./twitter.svg" alt="Twitter logo" />
 			<SocialLink href="https://github.com/ArtisanSelects/wordle-cheater" src="./github.svg" alt="Github logo" />
 		</div>
 	</div>
